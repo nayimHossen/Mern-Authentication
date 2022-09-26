a8cec8c4f834429e445bf28ddfbba59d67af7490
nayimHossen <nayimh2015@gmail.com>
2022-09-27 05:23:15 +0600

    header section

---
+++ frontend/src/componets/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="shadow-md">
+      <div className="navbar container mx-auto px-5">
+        <div className="flex-1">
+          <Link to="#" className="btn btn-ghost normal-case text-xl">
+            Hp Cloud
+          </Link>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" alt="" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="#" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="#">Settings</Link>
+              </li>
+              <li>
+                <Link to="#">Logout</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        serviceBg: "../assets/images/service-banner.png",
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3164DA",
+          secondary: "#355FD9",
+          accent: "#0E254E",
+          neutral: "#8d8d8d",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+      "cupcake",
+    ],
   },
   plugins: [require("daisyui")],
 };
